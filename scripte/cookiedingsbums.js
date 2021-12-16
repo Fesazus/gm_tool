9534f2a689fd9783ce0d4482e95498d19e7c2bfc
Fesazus <mail@eurador.de>
2021-12-16 17:06:31 +0100

    remove_cookies

--- scripte/cookiedingsbums.js
+++
@@ -1,6 +0,0 @@
-if(document.cookie.indexOf('hidecookiedingsbums=1') != -1){
-    jQuery('#cookiedingsbums').hide();
-} else {
-    jQuery('#cookiedingsbums').prependTo('body');
-    jQuery('#cookiedingsbumsCloser').show();
-}
